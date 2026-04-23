c591fe04f81182673419f06ffd00d2d2bc6f5b4e
FuthifoXx <futhifoxmaseko@gmail.com>
2026-04-23 17:34:05 +0200

    installed the CORS and enabled it and added the middleware on server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+
 dotenv.config()
 
 import connectDB from './config/db.js'
@@ -14,6 +16,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
+app.use(cors())
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+//   }),
+// )
 app.use(express.json())
 
 // Routes
